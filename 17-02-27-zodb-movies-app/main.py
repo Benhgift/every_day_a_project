294c891b2c439f5464d89c3920e5924ba03d62fb
benhgift <benhgift@gmail.com>
2017-02-27 20:36:39 -0800

    tested out zodb successfully

---
+++ 17-02-27-zodb-movies-app/main.py
@@ -0,0 +1,14 @@
+import ZODB
+import ZODB.FileStorage
+import transaction
+import BTrees.OOBTree
+import account
+
+storage = ZODB.FileStorage.FileStorage('mydata.fs')
+db = ZODB.DB(storage)
+connection = db.open()
+root = connection.root
+
+root.accounts = BTrees.OOBTree.BTree()
+root.accounts['account-1'] = account.Account('bob')
+transaction.commit()
